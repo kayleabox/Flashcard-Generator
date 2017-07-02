dd8af7eda85cf6d01b45c5f8fe333f82fd7f6c05
kaylea <kayleabox09@gmail.com>
2017-07-01 21:59:52 -0500

    edited text files and added a table at the end of the game

--- flashcards.js
+++ flashcards.js
@@ -98,7 +98,7 @@ function startGame(game){
         else{
             if(game != undefined){
                 console.log("thanks! you got "+game.correct+" right! come by again sometime!");
-                game.updateStats(game);
+                printTable(game);
             }
             else{console.log("thanks! come again sometime!");}
         }
@@ -115,25 +115,23 @@ function Game(questionList, username, theme){
         this.correct         = 0;
         this.incorrect       = 0;
         this.username        = username;
-        this.numberQuestions = Object.keys(questionList).lenth;
+        this.numberQuestions = Object.keys(questionList).length;
         this.updateStats = function(game){
             connection.query("UPDATE games SET ? WHERE ?", [{correct:game.correct, incorrect:game.incorrect}, {id: game.id}], 
             function(err, response){
                 //console.log("game "+game.id+" was updated"); //logs in the middle of startGame()
-                connection.query("SELECT * FROM games WHERE id =?", [game.id], function(err, response){
+                console.log("you played " + game.theme + " and got " + game.correct + " right of " + game.numberQuestions + "!");
+                connection.query("SELECT MAX(correct) FROM games WHERE theme =?", [game.theme] , function (err, response) {
                     if (err) throw err;
-                    console.log("you played " + response[0].theme + " and got " + response[0].correct + " right!");
-                    connection.query("SELECT MAX(correct) FROM games", function(err, response){
-                        if(err) throw err;
-                        if(game.correct >= response[0]["MAX(correct)"]){
-                            console.log("NICE! You have the highest score!");
-                        }
-                        else{
+                    if (game.correct >= response[0]["MAX(correct)"]) {
+                        console.log("NICE! You have the highest score!");
+                    }
+                    else {
                         console.log("The highest score was " + response[0]["MAX(correct)"]);
-                        }
-                        startGame();
-                    })
+                    }
+                    startGame(game);
                 })
+                
             })
         }
         this.logGame = function(username, game){
@@ -163,7 +161,7 @@ Game.prototype.play = function(game){
         message: question
     }]).then(function (answer, err) {
         if (err) throw err;
-        if (answer.answer == correctAnswer) {
+        if (answer.answer.toLowerCase() == correctAnswer.toLowerCase()) {
             console.log("way to go!");
             console.log(fulltext);
             game.count++;
@@ -186,9 +184,8 @@ Game.prototype.cardLoop = function(game){
         game.play(game);
     }
     else {
-        console.log("thanks! you got "+game.correct+" right! come by again sometime!");
+        console.log("You finished the game!");
         game.updateStats(game);
-        //startGame(game);
     }
 }
 
@@ -252,7 +249,7 @@ function createUser(){
         message: "enter your password"
     }]).then(function (create, err) {
         if(err) throw err;
-        if(create.username !== undefine && create.password !== undefined && create.password == create.confirm){
+        if(create.username !== undefined && create.password !== undefined && create.password == create.confirm){
             connection.query("SELECT * FROM users WHERE userName=? AND pword=?", [create.username, create.password], 
             function(err, response){
                 if(err) throw err;
@@ -276,6 +273,18 @@ function createUser(){
         else {
             console.log("All fields must be defined and passwords must match.")
             createUser();
+        }
+    })
+}
+
+function printTable(game){
+    connection.query("SELECT * FROM games WHERE theme = ?", [game.theme], function(err, response){
+        if(err) throw err;
+        console.log("     STATS FOR "+game.theme.toUpperCase());
+        console.log("---------------------------------");
+        console.log("  User      Correct   Incorrect  ")
+        for(i=0; i<response.length; i++){
+            console.log(response[i].userName + "       " + response[i].correct + "         " + response[i].incorrect);
         }
     })
 }
